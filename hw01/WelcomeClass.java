2f1e66a4721403d7b0daa5191ed36386f1a96464
zsl3203 <zsl3203@gmail.com>
2014-08-31 18:51:26 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////
+//Shaolun Zhang
+//hw 01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+//  print the statement with box around it
+    System.out.println("#############################");
+    System.out.println("#############################");
+    System.out.println("#        -----------        #");
+    System.out.println("#        | WELCOME |        #");
+    System.out.println("#        -----------        #");
+    System.out.println("#      ^  ^  ^  ^  ^  ^     #");
+    System.out.println("#     / \\/ \\/ \\/ \\/ \\/ \\    #");
+    System.out.println("#    |-S--H--Z--2--1--7-|   #");
+    System.out.println("#     \\ /\\ /\\ /\\ /\\ /\\ /    #");
+    System.out.println("#      v  v  v  v  v  v     #");
+    System.out.println("#  I love soccer and tennis #");
+    System.out.println("#############################");
+    System.out.println("#############################");
+    System.out.println("test");
+     
+    //test/testtest   
+  }
+}
