f3bcf10ab83d029bc0e7c6c6102eed5608d7235b
zsl3203 <zsl3203@gmail.com>
2014-09-12 02:11:59 +0000

    updated BigMac

---
+++ lab03-BigMac/BigMac.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//Shaolun Zhang
+//lab03
+//BigMac Java Programe
+//Arithmetic calculations and input
+//
+import java.util.Scanner;
+public class BigMac{
+    //main method
+    public static void main(String[] args) {
+        Scanner myScanner;
+        myScanner = new Scanner( System.in );
+        System.out.print("Enter the number of Big Macs(an integer > 0): ");
+        int nBigMacs = myScanner.nextInt();
+        System.out.print("Enter the cost per Big Mac as"
+        +" a double (in the form xx.xx): " );
+        double bigMac$ = myScanner.nextDouble();
+        System.out.print("Enter the percent tax as a whole number (xx): ");
+        double taxRate = myScanner.nextDouble();
+        taxRate/=100;
+        double cost$;
+        int dollars, dimes, pennies;
+        cost$=nBigMacs*bigMac$*(1+taxRate);
+        dollars=(int)cost$;
+        dimes=(int)(cost$*10)%10;
+        pennies=(int)(cost$*100)%10;
+        System.out.println("The total cost of "+nBigMacs+" BigMacs, at $"
+        +bigMac$+" per bigMac, with a sales tax of "+(int)(taxRate*100) 
+        +"%, is$"+dollars+'.'+dimes+pennies);
+    }
+}
